1546e5493e7fd04c904391bdf8ecb510e4be0d1e
Adam Basile <adam.basile@veitchlister.com.au>
2021-02-20 23:16:54 +1100

    Better names

--- dither.py
+++ dither.py
@@ -2,12 +2,12 @@ import numpy as np
 from skimage import io, color, transform, filters
 
 
-def slow_ed(img):
+def dither(img):
     current = np.zeros(np.array(img.shape) + 4)
     current[:] = np.nan
     current[2:-2, 2:-2] = img - 0.5
 
-    new = np.full_like(current, fill_value=np.nan)
+    dithered = np.full_like(current, fill_value=np.nan)
 
     kernel = np.zeros([5, 5])
     kernel[2, 2] = 1
@@ -19,23 +19,23 @@ def slow_ed(img):
         x, y = np.unravel_index(np.nanargmax(np.abs(current)), current.shape)
         val = current[x, y]
         current[x, y] = np.nan
-        new[x, y] = 0.5 + np.sign(val) / 2
+        dithered[x, y] = 0.5 + np.sign(val) / 2
         error = val - np.sign(val) / 2
         current[x - 2 : x + 3, y - 2 : y + 3] += error * kernel
-    return new[2:-2, 2:-2]
+    return dithered[2:-2, 2:-2]
 
 
 def read(path, size=None):
-    magnus = color.rgb2lab(io.imread(path)[:, :, :3])[:, :, 0] / 100
+    raw_img = color.rgb2lab(io.imread(path)[:, :, :3])[:, :, 0] / 100
     if size is None:
-        return magnus
+        return raw_img
     else:
-        return transform.resize(magnus, (size * np.array(magnus.shape[:2]) / max(magnus.shape[:2])).round())
+        return transform.resize(raw_img, (size * np.array(raw_img.shape[:2]) / max(raw_img.shape[:2])).round())
 
 
 def process(path, size, output_path):
-    magnus = read(path, size)
-    dithered = slow_ed(magnus)
+    img = read(path, size)
+    dithered = dither(img)
     io.imsave(output_path, 255 * dithered.astype("uint8"))
 
 
