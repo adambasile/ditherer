73e0410eb1a39c7bd1479c0f4844e917f1718803
Adam Basile <adam.basile@veitchlister.com.au>
2021-04-24 19:43:46 +1000

    Add 200 & 300 px tests

--- dither_test.py
+++ dither_test.py
@@ -7,7 +7,17 @@ import dither
 
 
 class TestDither(unittest.TestCase):
-    def test_dither(self):
+    def test_dither100(self):
         dithered = dither.dither(dither.read("example_images/circe.jpg", 100))
         expected = io.imread("example_images/100px.png")
+        np.testing.assert_equal(dithered, expected)
+
+    def test_dither200(self):
+        dithered = dither.dither(dither.read("example_images/circe.jpg", 200))
+        expected = io.imread("example_images/200px.png")
+        np.testing.assert_equal(dithered, expected)
+
+    def test_dither300(self):
+        dithered = dither.dither(dither.read("example_images/circe.jpg", 300))
+        expected = io.imread("example_images/300px.png")
         np.testing.assert_equal(dithered, expected)
