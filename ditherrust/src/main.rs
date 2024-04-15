5dffc26fb5e9bd9c85432020fdbd61744b448112
Adam Basile <basile.adam@gmail.com>
2024-04-15 16:10:53 +1000

    Rust working!

--- ditherrust/src/main.rs
+++ ditherrust/src/main.rs
@@ -1,10 +1,11 @@
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::env;
 use std::path::Path;
 
 use image::{ImageBuffer, Luma, open, Pixel, Rgb32FImage};
-use ndarray::Dimension;
-use ndarray::prelude::*;
+use image::imageops::FilterType;
+use ndarray::{arr2, Array, Array2, ArrayBase, Ix2, OwnedRepr, ShapeBuilder};
 use palette::{IntoColor, Lab, Srgb};
 use palette::white_point::D65;
 
@@ -18,8 +19,10 @@ fn main() {
 
     println!("Reading {:?}", infile);
     let raw_img = open(infile).unwrap();
+    println!("{:?} {:?}", raw_img.height(), raw_img.width());
 
-    let img = raw_img.to_rgb32f();
+    let img = raw_img.resize(100, 100, FilterType::Triangle).to_rgb32f();
+    println!("{:?} {:?}", img.height(), img.width());
 
     let luminance = convert_img_to_relative_luminance(&img);
 
@@ -30,9 +33,6 @@ fn main() {
     dithered.save(outfile).unwrap();
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
-
-
-
 }
 
 fn convert_luminance_to_int(img: &ImageBuffer<Luma<f32>, Vec<f32>>) -> ImageBuffer<Luma<u8>, Vec<u8>> {
@@ -54,7 +54,6 @@ fn convert_img_to_relative_luminance(img: &Rgb32FImage) -> ImageBuffer<Luma<f32>
 }
 
 
-
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Sign {
     Positive,
@@ -69,23 +68,86 @@ struct ErrorPixel {
     y: usize,
 }
 
+impl Eq for ErrorPixel {}
+
+impl Ord for ErrorPixel {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(&other).unwrap_or(Ordering::Less)
+    }
+}
+
+
 fn dither_img(img: &ImageBuffer<Luma<f32>, Vec<f32>>)
               -> ImageBuffer<Luma<u8>, Vec<u8>>
 {
-    let _kernel = arr2(&[[0.00354341, 0.01588048, 0.02618249, 0.01588048, 0.00354341],
+    let kernel = arr2(&[[0.00354341, 0.01588048, 0.02618249, 0.01588048, 0.00354341],
         [0.01588048, 0.07117138, 0.11734176, 0.07117138, 0.01588048],
         [0.02618249, 0.11734176, 0., 0.11734176, 0.02618249],
         [0.01588048, 0.07117138, 0.11734176, 0.07117138, 0.01588048],
         [0.00354341, 0.01588048, 0.02618249, 0.01588048, 0.00354341]]);
 
     let shape = (img.width() as usize, img.height() as usize).f();
-    let already_included = Array2::from_elem(shape, false);
-    let out = Array2::from_elem(shape, u8::MAX / 2);
-    let err_img = Array2::from_shape_vec(shape, img.to_vec()).unwrap() - Array2::from_elem(shape, 0.5);
+    let mut already_included = Array2::from_elem(shape, false);
+    let mut out = Array2::from_elem(shape, u8::MAX / 2);
+    let mut err_img: ArrayBase<OwnedRepr<f32>, Ix2> = Array2::from_shape_vec(shape, img.to_vec()).unwrap() - Array2::from_elem(shape, 50.0);
+    let mut errorheap = create_pixel_queue(&err_img);
+
+    while !errorheap.is_empty() {
+        let errorpixel = errorheap.pop().unwrap();
+        let xy = [errorpixel.x, errorpixel.y];
+        if already_included[xy] || (err_img[xy].abs() != errorpixel.error) {
+            continue;
+        }
+        already_included[xy] = true;
+        out[xy] = match errorpixel.sign {
+            Sign::Positive => { u8::MAX }
+            Sign::Negative => { 0 }
+        };
+        let error = &kernel * (50.0 - errorpixel.error) * match errorpixel.sign {
+            Sign::Positive => { -1.0 }
+            Sign::Negative => { 1.0 }
+        };
+        add_error(&mut err_img, error, xy);
+        errorheap = create_pixel_queue(&err_img);  // TODO: only do the pixels we've changed
+    }
     let out_img = ImageBuffer::from_fn(img.width(), img.height(), |x, y| {
         Luma([out[[x as usize, y as usize]]])
     });
     out_img
+}
+
+fn add_error(err_img: &mut ArrayBase<OwnedRepr<f32>, Ix2>, error: Array<f32, Ix2>, centre: [usize; 2]) {
+    let [error_width, error_height] = error.shape() else { unreachable!() };
+    let [centre_x, centre_y] = centre;
+    for i in 0..error_width.clone() {
+        for j in 0..error_height.clone() {
+            let x;
+            let y;
+            match (centre_x + i).checked_sub(error_width / 2) {
+                None => { continue; }
+                Some(val) => { x = val }
+            }
+            match (centre_y + j).checked_sub(error_height / 2) {
+                None => { continue; }
+                Some(val) => { y = val }
+            }
+            if (x + 1 > err_img.shape()[0]) || (y + 1 > err_img.shape()[1]) {
+                continue;
+            }
+            err_img[[x, y]] += error[[i, j]];
+        }
+    }
+}
+
+fn create_pixel_queue(err_img: &ArrayBase<OwnedRepr<f32>, Ix2>) -> BinaryHeap<ErrorPixel> {
+    let x1: Vec<ErrorPixel> = err_img.indexed_iter().map(|((x, y), error)| ErrorPixel {
+        error: error.abs(),
+        sign: if error.clone() < 0.0 { Sign::Negative } else { Sign::Positive },
+        x,
+        y,
+    }).collect();
+    let errorheap: BinaryHeap<ErrorPixel> = BinaryHeap::from(x1);
+    errorheap
 }
 
 
