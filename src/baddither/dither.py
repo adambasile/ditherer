143c8736722745a8d5f9c018adb2869f964b5944
Adam Basile <basile.adam@gmail.com>
2026-02-18 23:19:25 +1100

    Shift to src/ format

--- src/baddither/dither.py
+++ src/baddither/dither.py
@@ -80,16 +80,3 @@ def process(path, size, output_path):
     img = read(path, size)
     dithered = dither(img)
     io.imsave(output_path, dithered)
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Dither an image.")
-    parser.add_argument("input", type=str, help="path of image to dither")
-    parser.add_argument("output", type=str, help="output path")
-    parser.add_argument(
-        "-s", "--size", type=int, help="Proportionally resize largest dimension of image to this value before dithering"
-    )
-    args = parser.parse_args()
-    process(args.input, args.size, args.output)
